5eaae8539e5074a9681d92f20287337c145feddf
Esayas F. Gebresilasie <addiscodr@gmail.com>
2026-05-06 10:38:40 +0300

    Add google-services.json to code base; Modifyapp/ build.gradle.kts,android/build.gradle.kts, main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:package_delivery_solution/pages/home_page.dart';
 import 'package:package_delivery_solution/pages/onboarding.dart';
 import 'package:package_delivery_solution/pages/post_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
